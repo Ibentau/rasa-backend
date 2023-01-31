1863afd34405e68feef0f6b00bfd1e5dbf76f3ec
Pixselve <38262536+Pixselve@users.noreply.github.com>
2023-01-31 11:47:02 +0100

    feat: working

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,51 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+# make a function that reads th "config.json"
+# {
+#   "event_start": "2019-01-01T00:00:00Z",
+#   "event_end": "2019-01-01T00:00:00Z",
+#   "talks": [
+#     {
+#       "speaker": "John Doe",
+#       "start": "2018-01-01T00:00:00Z",
+#       "end": "2018-01-01T00:00:00Z",
+#       "title": "Talk Title"
+#     }
+#   ]
+# }
+def read_config():
+    import json
+    with open("config.json") as f:
+        config = json.load(f)
+    return config
+
+a = read_config()
+print(a)
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        speakerName = tracker.slots["speaker_name"]
+        # find the speaker name in the config.json
+        # if found, return "SPEAKER NAME is speaking at TIME and TITLE"
+        # if not found, return "I don't know"
+        for talk in a["talks"]:
+            if talk["speaker"] == speakerName:
+                dispatcher.utter_message(text=f"{speakerName} is speaking at {talk['start']} and {talk['title']}")
+                return []
+        dispatcher.utter_message(text="I don't know")
+        return []
+
+        return []
