3eac86afab282b650f6e21f1d60292b0983c24ab
Pixselve <38262536+Pixselve@users.noreply.github.com>
2023-02-07 08:38:41 +0100

    feat: event time

--- actions/actions.py
+++ actions/actions.py
@@ -8,6 +8,7 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import datetime
 
 def read_config():
     import json
@@ -47,4 +48,36 @@ class ActionAddress(Action):
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
         dispatcher.utter_message(text="The venue is located at "+a["address"])
+        return []
+
+class ActionTime(Action):
+
+    def name(self) -> Text:
+        return "action_time"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        event_start = a["event_start"]
+        event_end = a["event_end"]
+        # parse the event_start and event_end to datetime
+        # compare the current datetime to event_start and event_end
+        # if current time is before event_start, return "The event is starting at TIME"
+        # if current time is after event_start and before event_end, return "The event is ongoing"
+        # if current time is after event_end, return "The event has ended"
+
+        current_time = datetime.datetime.now()
+        event_start_time = datetime.datetime.strptime(event_start, "%Y-%m-%d %H:%M")
+        event_end_time = datetime.datetime.strptime(event_end, "%Y-%m-%d %H:%M")
+        if current_time < event_start_time:
+            dispatcher.utter_message(text="The event is starting at "+event_start)
+        elif current_time > event_start_time and current_time < event_end_time:
+            dispatcher.utter_message(text="The event is ongoing")
+        else:
+            dispatcher.utter_message(text="The event has ended")
+        return []
+
+
+        dispatcher.utter_message(text="The conference starts at "+a["start_time"])
         return []
