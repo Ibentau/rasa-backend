5dbceb300c461ca3a574501338377ab0faf18107
albarry <alion.barry18@inphb.ci>
2023-02-07 08:09:23 +0100

    address_action

--- actions/actions.py
+++ actions/actions.py
@@ -3,28 +3,12 @@
 #
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
-
-
 # This is a simple example for a custom action which utters "Hello World!"
 
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
-# make a function that reads th "config.json"
-# {
-#   "event_start": "2019-01-01T00:00:00Z",
-#   "event_end": "2019-01-01T00:00:00Z",
-#   "talks": [
-#     {
-#       "speaker": "John Doe",
-#       "start": "2018-01-01T00:00:00Z",
-#       "end": "2018-01-01T00:00:00Z",
-#       "title": "Talk Title"
-#     }
-#   ]
-# }
 def read_config():
     import json
     with open("config.json") as f:
@@ -32,7 +16,6 @@ def read_config():
     return config
 
 a = read_config()
-print(a)
 
 class ActionHelloWorld(Action):
 
@@ -54,4 +37,14 @@ class ActionHelloWorld(Action):
         dispatcher.utter_message(text="I don't know")
         return []
 
+class ActionAddress(Action):
+
+    def name(self) -> Text:
+        return "action_address"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="The venue is located at "+a["place"])
         return []
