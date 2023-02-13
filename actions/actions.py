2ebc9b95e8170d4f8307a78fd62212185945b0d5
albarry <alion.barry18@inphb.ci>
2023-02-13 23:35:15 +0100

    gestion_casse_et_accent, minimisation_des_fautes

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,6 @@
 # This files contains your custom actions which can be used to run
 # custom Python code.
-#
+
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 # This is a simple example for a custom action which utters "Hello World!"
@@ -8,6 +8,8 @@
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+import Levenshtein
+from unidecode import unidecode
 
 def read_config():
     import json
@@ -25,18 +27,30 @@ class ActionHelloWorld(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        speaker_list={}
+        counter=0
+        # Get the Input data
+        # throw 'emphases'
+        # lower all the input data
+        speakerName = unidecode(tracker.slots["speaker_name"]).lower()
 
-        speakerName = tracker.slots["speaker_name"]
         # find the speaker name in the config.json
         # if found, return "SPEAKER NAME is speaking at TIME and TITLE"
         # if not found, return "I don't know"
-        for talk in a["talks"]:
-            if talk["speaker"] == speakerName:
-                dispatcher.utter_message(text=f"{speakerName} is speaking at {talk['start']} and {talk['title']}")
-                return []
-        dispatcher.utter_message(text="I don't know")
-        return []
 
+        for talk in a["talks"]:
+            distance=Levenshtein.distance(speakerName,unidecode(talk["speaker"]).lower())
+            if distance<3:
+                speaker_list[distance]=talk["speaker"]+" is speaking at"+talk['start']+ " about "+ talk['title']
+                counter+=1
+        if counter!=0:
+            min_key=min(speaker_list.keys())
+            dispatcher.utter_message(speaker_list[min_key])
+        else:
+            dispatcher.utter_message(text="I don't know, Maybe you have mispelled the name of the speaker try again ")        
+        return[]
+        
 class ActionAddress(Action):
 
     def name(self) -> Text:
@@ -45,6 +59,5 @@ class ActionAddress(Action):
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
         dispatcher.utter_message(text="The venue is located at "+a["place"])
         return []
