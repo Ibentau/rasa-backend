1843bb8923c56bbf19678d747b61886275e0240f
Zié Amara <zie.traore18@inphb.ci>
2023-03-28 17:47:19 +0200

    Action for registration office hours

--- actions/actions.py
+++ actions/actions.py
@@ -19,7 +19,7 @@ import pytz
 
 def read_config():
     import json
-    with open("config.json") as f:
+    with open("config.example.json") as f:
         config = json.load(f)
     return config
 
@@ -296,4 +296,24 @@ class ActionNextMeal(Action):
         else:
             dispatcher.utter_message(response="utter_no_next_meal")
 
+        return []
+
+class ActionRegistrationOfficeHours(Action):
+
+    def name(self) -> Text:
+        return "action_registration_office_hours"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # find the hour_start and hour_end of the registration
+        registration_start = json_config["registration"]["start"]
+        registration_end = json_config["registration"]["end"]
+
+        registration_start = datetime.datetime.strptime(registration_start, "%Y-%m-%dT%H:%M:%SZ")
+        registration_end = datetime.datetime.strptime(registration_end, "%Y-%m-%dT%H:%M:%SZ")
+
+        dispatcher.utter_message(response="utter_registration_office_hours", hour_start=registration_start.strftime("%H:%M:%S"), hour_end=registration_end.strftime("%H:%M:%S"), registration_start=registration_start.strftime("%A, %d %B %Y"), registration_end=registration_end.strftime('%A, %d %B %Y'))
+        
         return []
